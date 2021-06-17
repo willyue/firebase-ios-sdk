7666e53231f95b98962b625888fb1006a8a4c249
Rosalyn Tan <rosalyntan@google.com>
2021-06-17 16:21:58 -0700

    Handle when no refresh token is returned the backend.

--- FirebaseAuth/Sources/User/FIRUser.m
+++ FirebaseAuth/Sources/User/FIRUser.m
@@ -282,28 +282,34 @@ static void callInMainThreadWithAuthDataResultAndError(
   user.auth = auth;
   user.tenantID = auth.tenantID;
   user.requestConfiguration = auth.requestConfiguration;
-  [user internalGetTokenWithCallback:^(NSString *_Nullable accessToken, NSError *_Nullable error) {
-    if (error) {
-      callback(nil, error);
-      return;
-    }
-    FIRGetAccountInfoRequest *getAccountInfoRequest =
-        [[FIRGetAccountInfoRequest alloc] initWithAccessToken:accessToken
-                                         requestConfiguration:auth.requestConfiguration];
-    [FIRAuthBackend
-        getAccountInfo:getAccountInfoRequest
-              callback:^(FIRGetAccountInfoResponse *_Nullable response, NSError *_Nullable error) {
-                if (error) {
-                  // No need to sign out user here for errors because the user hasn't been signed in
-                  // yet.
-                  callback(nil, error);
-                  return;
-                }
-                user.anonymous = anonymous;
-                [user updateWithGetAccountInfoResponse:response];
-                callback(user, nil);
-              }];
-  }];
+  if (refreshToken.length) {
+    [user
+        internalGetTokenWithCallback:^(NSString *_Nullable accessToken, NSError *_Nullable error) {
+          if (error) {
+            callback(nil, error);
+            return;
+          }
+          FIRGetAccountInfoRequest *getAccountInfoRequest =
+              [[FIRGetAccountInfoRequest alloc] initWithAccessToken:accessToken
+                                               requestConfiguration:auth.requestConfiguration];
+          [FIRAuthBackend getAccountInfo:getAccountInfoRequest
+                                callback:^(FIRGetAccountInfoResponse *_Nullable response,
+                                           NSError *_Nullable error) {
+                                  if (error) {
+                                    // No need to sign out user here for errors because the user
+                                    // hasn't been signed in yet.
+                                    callback(nil, error);
+                                    return;
+                                  }
+                                  user.anonymous = anonymous;
+                                  [user updateWithGetAccountInfoResponse:response];
+                                  callback(user, nil);
+                                }];
+        }];
+  } else {
+    [user updateWithIDToken:accessToken];
+    callback(user, nil);
+  }
 }
 
 - (instancetype)initWithTokenService:(FIRSecureTokenService *)tokenService {
@@ -468,6 +474,12 @@ static void callInMainThreadWithAuthDataResultAndError(
   _multiFactor = [[FIRMultiFactor alloc] initWithMFAEnrollments:user.MFAEnrollments];
   _multiFactor.user = self;
 #endif
+}
+
+- (void)updateWithIDToken:(NSString *)token {
+  FIRAuthTokenResult *tokenResult = [FIRAuthTokenResult tokenResultWithToken:token];
+  NSDictionary *tokenClaims = tokenResult.claims;
+  _userID = tokenClaims[@"uid"];
 }
 
 /** @fn executeUserUpdateWithChanges:callback:
